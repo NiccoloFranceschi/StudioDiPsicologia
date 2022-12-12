924a03e13e962dbc0e7ad7b377ac06819d170ae9
Niccolò Franceschi <Niccol Franceschi@DESKTOP-H3536B9>
2022-12-12 21:11:55 +0100

    Aggiungere i file di progetto.

---
+++ StudioDiPsicologia/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace StudioDiPsicologia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblSquadre = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMedici = new System.Windows.Forms.Label();
+            this.lblStadi = new System.Windows.Forms.Label();
+            this.lblPartite = new System.Windows.Forms.Label();
+            this.lblHome = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(47)))), ((int)(((byte)(54)))));
+            this.panel3.Controls.Add(this.lblSquadre);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.lblMedici);
+            this.panel3.Controls.Add(this.lblStadi);
+            this.panel3.Controls.Add(this.lblPartite);
+            this.panel3.Controls.Add(this.lblHome);
+            this.panel3.Location = new System.Drawing.Point(12, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(945, 40);
+            this.panel3.TabIndex = 14;
+            // 
+            // lblSquadre
+            // 
+            this.lblSquadre.AutoSize = true;
+            this.lblSquadre.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblSquadre.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblSquadre.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSquadre.ForeColor = System.Drawing.Color.White;
+            this.lblSquadre.Location = new System.Drawing.Point(376, 0);
+            this.lblSquadre.Name = "lblSquadre";
+            this.lblSquadre.Size = new System.Drawing.Size(156, 25);
+            this.lblSquadre.TabIndex = 12;
+            this.lblSquadre.Text = "Appuntamenti";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(714, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 31);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Studio Psicology";
+            // 
+            // lblMedici
+            // 
+            this.lblMedici.AutoSize = true;
+            this.lblMedici.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblMedici.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblMedici.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedici.ForeColor = System.Drawing.Color.White;
+            this.lblMedici.Location = new System.Drawing.Point(295, 0);
+            this.lblMedici.Name = "lblMedici";
+            this.lblMedici.Size = new System.Drawing.Size(81, 25);
+            this.lblMedici.TabIndex = 11;
+            this.lblMedici.Text = "Medici";
+            // 
+            // lblStadi
+            // 
+            this.lblStadi.AutoSize = true;
+            this.lblStadi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblStadi.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblStadi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStadi.ForeColor = System.Drawing.Color.White;
+            this.lblStadi.Location = new System.Drawing.Point(198, 0);
+            this.lblStadi.Name = "lblStadi";
+            this.lblStadi.Size = new System.Drawing.Size(97, 25);
+            this.lblStadi.TabIndex = 8;
+            this.lblStadi.Text = "Pazienti";
+            // 
+            // lblPartite
+            // 
+            this.lblPartite.AutoSize = true;
+            this.lblPartite.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblPartite.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblPartite.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPartite.ForeColor = System.Drawing.Color.White;
+            this.lblPartite.Location = new System.Drawing.Point(72, 0);
+            this.lblPartite.Name = "lblPartite";
+            this.lblPartite.Size = new System.Drawing.Size(126, 25);
+            this.lblPartite.TabIndex = 10;
+            this.lblPartite.Text = "Calendario";
+            // 
+            // lblHome
+            // 
+            this.lblHome.AutoSize = true;
+            this.lblHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblHome.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHome.ForeColor = System.Drawing.Color.White;
+            this.lblHome.Location = new System.Drawing.Point(0, 0);
+            this.lblHome.Name = "lblHome";
+            this.lblHome.Size = new System.Drawing.Size(72, 25);
+            this.lblHome.TabIndex = 9;
+            this.lblHome.Text = "Home";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(52)))));
+            this.ClientSize = new System.Drawing.Size(969, 495);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Psicology";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblSquadre;
+        private System.Windows.Forms.Label lblMedici;
+        private System.Windows.Forms.Label lblStadi;
+        private System.Windows.Forms.Label lblPartite;
+        private System.Windows.Forms.Label lblHome;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
